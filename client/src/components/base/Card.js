c34c8225917449446bbd1af6fc7d85f4a3bef501
Malinicz <malinicz@gmail.com>
2019-02-14 22:50:55 +0100

    style fix

--- client/src/components/base/Card.js
+++ client/src/components/base/Card.js
@@ -4,7 +4,7 @@ export const Card = styled.div`
   position: relative;
   padding: ${({ theme }) => theme.baseSpacing * 4}px;
   max-width: 550px;
-  border-radius: 8%;
+  border-radius: 30px;
   background-color: ${({ theme }) => theme.colors.brightest};
   box-shadow: 0px 6px 15px 0px rgba(0, 0, 0, 0.06);
 
